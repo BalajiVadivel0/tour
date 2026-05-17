f834a7a7871095f01e34e89782333e2602540589
Balaji V <balajivadivel05@gmail.com>
2026-05-17 16:17:12 +0530

    fix

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:5000/api" });
+const API = axios.create({ baseURL: process.env.REACT_APP_API_URL || "http://localhost:5000/api" });
 
 // 🔑 Add token automatically if logged in
 API.interceptors.request.use((req) => {
